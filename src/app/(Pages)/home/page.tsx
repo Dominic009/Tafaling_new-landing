bde673ff750e4da59395a466e65fd4418736f0d6
Dominic009 <dpg11000@gmail.com>
2024-08-31 17:12:20 +0600

    Dynamic navbar implemented

--- src/app/(Pages)/home/page.tsx
+++ src/app/(Pages)/home/page.tsx
@@ -5,33 +5,6 @@ import React from "react";
 
 const page = () => {
   return (
-    // <div>
-    //   <div className='fixed z-50 w-full'>
-    //     <Navbar></Navbar>
-    //   </div>
-    //   {/* Page Layout */}
-    //   <div className='w-full grid gap-3 text-center justify-center px-8 py-20'>
-    //     <div className='border h-[80vh] fixed left-20 w-[18%]'>
-    //       Left Section
-    //     </div>
-
-    //     <div className='w-[900px]'>
-    //       {/* Create Post section */}
-    //       <div>
-    //         <Post></Post>
-    //       </div>
-    //       {/* User Posts */}
-    //       <div>
-    //         <UserPost></UserPost>
-    //       </div>
-    //     </div>
-
-    //     <div className='border h-[80vh] fixed right-20 w-[18%]'>
-    //       Right Section
-    //     </div>
-    //   </div>
-    // </div>
-
     <div>
       {/* Page Layout */}
       <div className="grid lg:grid-cols-4 gap-2 w-full md:w-[90%] lg:w-[80%] mx-auto px-5 text-center">
