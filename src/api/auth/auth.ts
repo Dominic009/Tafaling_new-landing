a3b72b08ca712abf8468c70626b2a333ec288396
Sunit Corneleous <sunit.corneleous25@gmail.com>
2024-09-01 11:24:21 +0600

    Type folder added

--- src/api/auth/auth.ts
+++ src/api/auth/auth.ts
@@ -1,9 +1,5 @@
+import { AuthUser } from '@/types/Auth';
 import axiosClient from '../config';
-
-export type AuthUser = {
-  email: string;
-  password: string;
-};
 
 export async function loginUser(data: AuthUser) {
   return await axiosClient.post('auth/login', JSON.stringify(data));
