9f034a4a7bcacb9b420b7e1967f44e02ea50e757
Dominic009 <dpg11000@gmail.com>
2024-09-02 11:37:13 +0600

    Register conflict

--- src/app/(Pages)/register/page.tsx
+++ src/app/(Pages)/register/page.tsx
@@ -1,19 +1,9 @@
-<<<<<<< HEAD
+import { registerUser } from "@/api/auth/auth";
 import PrimaryBtn from "@/components/PrimaryBtn";
+import { AuthUser } from "@/types/Auth";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
-=======
-"use client";
-import PrimaryBtn from "@/components/PrimaryBtn";
-import Image from "next/image";
-import Link from "next/link";
-import { AuthUser } from "@/types/Auth";
-import { registerUser } from "@/api/auth/auth";
-import { useRouter } from "next/navigation";
-const Page = () => {
-  const router = useRouter();
->>>>>>> 7cbcff45ade6769f92a23d6313feae48b7f6387d
 
   const handleRegisterUser = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -39,11 +29,7 @@ const Page = () => {
       <div className="opacity-20 absolute -left-52 scale-125"></div>
 
       <div
-<<<<<<< HEAD
         className="grid grid-cols-1 lg:grid-cols-2 gap-72 md:gap-0 w-full items-center justify-center"
-=======
-        className="grid grid-cols-1 lg:grid-cols-2 w-full items-center justify-center"
->>>>>>> 7cbcff45ade6769f92a23d6313feae48b7f6387d
         style={{
           backgroundImage: `url('/Pattern 3.png')`,
           backgroundSize: "1700px",
@@ -51,50 +37,31 @@ const Page = () => {
           backgroundRepeat: "no-repeat",
         }}
       >
-<<<<<<< HEAD
         {/* Right side */}
         <div className="z-50 flex flex-col items-center justify-center lg:border-l scale-50 lg:scale-100 h-[50%] lg:order-last md:-mt-0">
           <h1 className="font-semibold text-[#08B7EB] text-4xl mb-3">
             Welcome to
           </h1>
 
-=======
-        {/* Left side */}
-        <div className="z-50 flex flex-col items-center justify-center lg:border-l scale-50 lg:scale-100 h-[50%] lg:order-last -mt-36 md:-mt-0">
-          <h1 className="font-semibold text-[#08B7EB] text-4xl mb-3">
-            Welcome to
-          </h1>
->>>>>>> 7cbcff45ade6769f92a23d6313feae48b7f6387d
           <Link href={"/"}>
             <Image
               src="/Tafaling logo.png"
               alt="Image"
               width={500}
               height={200}
-<<<<<<< HEAD
               style={{ width: "100%", height: "auto" }}
-=======
-              style={{ width: "80%", height: "auto" }}
-              className="mix-blend-plus-darker"
->>>>>>> 7cbcff45ade6769f92a23d6313feae48b7f6387d
             ></Image>
           </Link>
         </div>
 
-<<<<<<< HEAD
         {/* Left side */}
         <div className="w-[90%] md:w-[70%] lg:max-w-[65%] mx-auto z-50 -mt-80 md:-mt-0">
-=======
-        {/* Rigth side */}
-        <div className="w-[90%] md:w-[70%] lg:max-w-[50%] mx-auto z-50 -mt-80 md:-mt-0">
->>>>>>> 7cbcff45ade6769f92a23d6313feae48b7f6387d
           <div className="bg-gray-900/30 py-10 rounded-xl flex flex-col items-center backdrop-blur-sm">
             <h1 className="text-3xl text-white border-b-4 border-[#008EAD] mb-8">
               Sign Up
             </h1>
 
             {/* Input fields */}
-<<<<<<< HEAD
             <div className="flex flex-col gap-7 w-[80%]">
               {/* Name */}
               <input
@@ -154,74 +121,6 @@ const Page = () => {
               size={"2xl"}
               weight={"bold"}
             />
-=======
-            <form onSubmit={handleRegisterUser}>
-              <div className="flex flex-col gap-7 w-[80%]">
-                {/* Name */}
-                <input
-                  placeholder="Your Name"
-                  name="fullName"
-                  type="text"
-                  className="px-4 py-2 rounded-md outline-none"
-                ></input>
-                {/* Email */}
-                <input
-                  placeholder="Your Email"
-                  type="text"
-                  name="email"
-                  className="px-4 py-2 rounded-md outline-none"
-                ></input>
-                {/* Password */}
-                <div className="relative">
-                  <input
-                    placeholder="Your Password"
-                    type="password"
-                    name="password"
-                    className="px-4 py-2 rounded-md outline-none w-full"
-                  ></input>
-                  <Image
-                    src={"/Eye.png"}
-                    alt="Eye_Icon"
-                    width={20}
-                    height={20}
-                    className="absolute right-3 top-3"
-                  ></Image>
-                </div>
-                {/* Confirm Password */}
-                <input
-                  placeholder="Confirm Password"
-                  type="password"
-                  name="confirmPassword"
-                  className="px-4 py-2 rounded-md outline-none"
-                ></input>
-              </div>
-
-              {/* Sign In button */}
-              <div className="flex items-center gap-2 w-[80%] mt-8 mb-2">
-                <input type="checkbox" />
-                <p className="text-[#D6EAFF]/50">
-                  Accept
-                  <a href="#" className="text-gray-300">
-                    {" "}
-                    Terms & Conditions
-                  </a>{" "}
-                  and{" "}
-                  <a href="#" className="text-gray-300">
-                    Privacy & Policy
-                  </a>
-                </p>
-              </div>
-
-              {/* Sign in btn */}
-              <PrimaryBtn
-                text={"Sign Up"}
-                width={"80%"}
-                size={"2xl"}
-                weight={"bold"}
-                type="submit"
-              />
-            </form>
->>>>>>> 7cbcff45ade6769f92a23d6313feae48b7f6387d
           </div>
 
           {/* Join Now link */}
