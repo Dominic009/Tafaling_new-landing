25a5bcc7daaed75910c071d940cd7eef2fe4cb55
Sunit Corneleous <sunit.corneleous25@gmail.com>
2024-09-09 11:48:38 +0600

    minor change

--- src/api/auth/auth.ts
+++ src/api/auth/auth.ts
@@ -4,9 +4,9 @@ import { AxiosResponse } from 'axios';
 
 export async function loginUser(
   data: AuthUser
-): Promise<AxiosResponse<any, any>> {
+): Promise<AxiosResponse<any, ResponseType>> {
   return await axiosClient.post('auth/login', JSON.stringify(data));
 }
-export async function registerUser(data: AuthUser) {
+export async function registerUser(data: AuthUser): Promise<AxiosResponse<any, ResponseType>> {
   return await axiosClient.post('auth/register', JSON.stringify(data));
 }
