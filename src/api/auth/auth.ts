dda4d8726c98221fdefae6a74b3d55ef496210b7
SUNIT <sunit.corneleous25@gmail.com>
2024-09-16 11:23:38 +0600

    refetch user on refresh for private route

--- src/api/auth/auth.ts
+++ src/api/auth/auth.ts
@@ -7,6 +7,19 @@ export async function loginUser(
 ): Promise<AxiosResponse<any, ResponseType>> {
   return await axiosClient.post('auth/login', JSON.stringify(data));
 }
-export async function registerUser(data: AuthUser): Promise<AxiosResponse<any, ResponseType>> {
+export async function registerUser(
+  data: AuthUser
+): Promise<AxiosResponse<any, ResponseType>> {
   return await axiosClient.post('auth/register', JSON.stringify(data));
+}
+
+// refetch user data
+export async function getAuthUser(
+  token: string
+): Promise<AxiosResponse<any, ResponseType>> {
+  return await axiosClient.get('auth/user', {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
 }
