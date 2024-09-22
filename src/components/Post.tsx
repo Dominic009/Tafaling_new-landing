ec8da75e921b7b4a99eb84333cee2022115faa99
Sunit Corneleous <sunit.corneleous25@gmail.com>
2024-09-22 16:05:48 +0600

    comment hide

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,10 +1,10 @@
-"use client";
-import Image from "next/legacy/image";
-import React, { useState } from "react";
-import Modal from "./Modal/Modal";
-import PostBody from "./PostBody";
-import FileUploader from "./Input File/FileUploader";
-import { useAuth } from "@/context/AuthContext/AuthProvider";
+'use client';
+import Image from 'next/legacy/image';
+import React, { useState } from 'react';
+import Modal from './Modal/Modal';
+import PostBody from './PostBody';
+import FileUploader from './Input File/FileUploader';
+import { useAuth } from '@/context/AuthContext/AuthProvider';
 
 const Post: React.FC = () => {
   const [modal, setModal] = useState<boolean>(false);
@@ -13,7 +13,7 @@ const Post: React.FC = () => {
 
   // Function to handle the opening of the modal for different tabs
   const openModalForTab = (tab: string | null) => {
-    setActiveTab(tab); 
+    setActiveTab(tab);
     setModal(true); // Open the modal
   };
 
@@ -23,66 +23,66 @@ const Post: React.FC = () => {
     setActiveTab(null);
   };
 
-  console.log(activeTab)
+  // console.log(activeTab)
 
   return (
     <div>
-      <div className="w-full mx-auto backdrop-blur-md rounded-xl p-3 shadow mb-6 bg-white">
+      <div className='w-full mx-auto backdrop-blur-md rounded-xl p-3 shadow mb-6 bg-white'>
         <div>
           {/* User Profile and Post Button */}
-          <div className="flex items-center gap-3 ">
+          <div className='flex items-center gap-3 '>
             <Image
-              alt="User DP"
-              src={ user?.profile_picture || '/ProfileDP/Dummy.png'}
+              alt='User DP'
+              src={user?.profile_picture || '/ProfileDP/Dummy.png'}
               width={50}
               height={50}
-              className="mt-1 rounded-full"
+              className='mt-1 rounded-full'
             />
             <button
               onClick={() => openModalForTab(null)}
-              className="text-gray-400 font-light w-full outline-none bg-gray-100 px-4 py-2 rounded-full text-left transition duration-300 ease-in-out"
+              className='text-gray-400 font-light w-full outline-none bg-gray-100 px-4 py-2 rounded-full text-left transition duration-300 ease-in-out'
             >
               Thinking about something...?
             </button>
           </div>
 
-          <div className="border-b w-full mt-2 mb-2"></div>
+          <div className='border-b w-full mt-2 mb-2'></div>
 
           {/* Interaction Buttons */}
-          <ul className="grid grid-cols-3 text-gray-400">
+          <ul className='grid grid-cols-3 text-gray-400'>
             <li
-              onClick={() => openModalForTab("photo-video")}
-              className="hover:bg-gray-100 px-4 rounded-full cursor-pointer flex items-center justify-center gap-1"
+              onClick={() => openModalForTab('photo-video')}
+              className='hover:bg-gray-100 px-4 rounded-full cursor-pointer flex items-center justify-center gap-1'
             >
               <Image
-                src={"/Icons/media.png"}
+                src={'/Icons/media.png'}
                 width={30}
                 height={30}
-                alt="Media icon"
+                alt='Media icon'
               />
               Photo/Video
             </li>
             <li
-              onClick={() => openModalForTab("location")}
-              className="hover:bg-gray-100 px-4 py-1 rounded-full cursor-pointer flex items-center justify-center gap-1"
+              onClick={() => openModalForTab('location')}
+              className='hover:bg-gray-100 px-4 py-1 rounded-full cursor-pointer flex items-center justify-center gap-1'
             >
               <Image
-                src={"/Icons/location.png"}
+                src={'/Icons/location.png'}
                 width={30}
                 height={30}
-                alt="Location icon"
+                alt='Location icon'
               />
               Location
             </li>
             <li
-              onClick={() => openModalForTab("activity")}
-              className="hover:bg-gray-100 px-4 py-1 rounded-full cursor-pointer flex items-center justify-center gap-1"
+              onClick={() => openModalForTab('activity')}
+              className='hover:bg-gray-100 px-4 py-1 rounded-full cursor-pointer flex items-center justify-center gap-1'
             >
               <Image
-                src={"/Icons/emoji.png"}
+                src={'/Icons/emoji.png'}
                 width={30}
                 height={30}
-                alt="Activity icon"
+                alt='Activity icon'
               />
               Activity
             </li>
@@ -96,7 +96,7 @@ const Post: React.FC = () => {
         {!activeTab && <PostBody></PostBody>}
 
         {/* Modal for Photo/Video Upload */}
-        {activeTab === "photo-video" && (
+        {activeTab === 'photo-video' && (
           <div>
             <PostBody>
               {/* old */}
@@ -129,30 +129,30 @@ const Post: React.FC = () => {
         )}
 
         {/* Modal for Location */}
-        {activeTab === "location" && (
+        {activeTab === 'location' && (
           <PostBody>
-            {" "}
+            {' '}
             <div>
-              <h2 className="text-xl mb-4">Add Location</h2>
+              <h2 className='text-xl mb-4'>Add Location</h2>
               <input
-                type="text"
-                placeholder="Enter location..."
-                className="w-full border border-gray-300 p-2 rounded-md mb-4"
+                type='text'
+                placeholder='Enter location...'
+                className='w-full border border-gray-300 p-2 rounded-md mb-4'
               />
             </div>
           </PostBody>
         )}
 
         {/* Modal for Activity */}
-        {activeTab === "activity" && (
+        {activeTab === 'activity' && (
           <PostBody>
-            {" "}
+            {' '}
             <div>
-              <h2 className="text-xl mb-4">Add Activity</h2>
+              <h2 className='text-xl mb-4'>Add Activity</h2>
               <input
-                type="text"
-                placeholder="What are you doing?"
-                className="w-full border border-gray-300 p-2 rounded-md mb-4"
+                type='text'
+                placeholder='What are you doing?'
+                className='w-full border border-gray-300 p-2 rounded-md mb-4'
               />
             </div>
           </PostBody>
