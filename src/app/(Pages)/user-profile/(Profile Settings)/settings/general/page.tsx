6726d48113fc43bf7ef59f896488fddfebd95f7f
Sunit Corneleous <sunit.corneleous25@gmail.com>
2024-09-22 15:45:24 +0600

    profile settings bug fix

--- src/app/(Pages)/user-profile/(Profile Settings)/settings/general/page.tsx
+++ src/app/(Pages)/user-profile/(Profile Settings)/settings/general/page.tsx
@@ -1,17 +1,19 @@
-import ChangePassword from "@/components/ChangePassword";
-import Heading from "@/components/Headings/Settings Headings/Heading";
-import React from "react";
+'use client';
+import ChangePassword from '@/components/ChangePassword';
+import Heading from '@/components/Headings/Settings Headings/Heading';
+import PrivateRoute from '@/components/PrivateRoute/PrivateRoute';
+import React from 'react';
 
 const page = () => {
   return (
     <div>
       {/* Change password */}
-      <div className="border-b py-2">
-        <Heading heading="Change Password" />
+      <div className='border-b py-2'>
+        <Heading heading='Change Password' />
         <ChangePassword />
       </div>
     </div>
   );
 };
 
-export default page;
+export default PrivateRoute(page);
