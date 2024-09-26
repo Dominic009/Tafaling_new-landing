b5268ae72472726390622e118533f2b38ef3b9e2
Sunit Corneleous <sunit.corneleous25@gmail.com>
2024-09-26 18:00:44 +0600

    user post updated

--- src/components/UserPost.tsx
+++ src/components/UserPost.tsx
@@ -1,139 +1,3 @@
-// "use client";
-// import Image from "next/legacy/image";
-// import React, { useEffect, useState } from "react";
-// import { HiDotsHorizontal } from "react-icons/hi";
-// import { IoLocationOutline } from "react-icons/io5";
-// import PostTimeConverter from "./PostTimeConverter";
-// import ContentViewer from "./Content Viewer/ContentViewer";
-
-// interface Post {
-//   profilePicture: string;
-//   username: string;
-//   location: string;
-//   contentType: string;
-//   postContent: string;
-//   caption: string;
-//   hashtags: string[];
-//   postedTime: string;
-// }
-
-// const UserPost: React.FC = () => {
-//   const [posts, setPosts] = React.useState<Post[]>([]);
-//   const [viewImagePost, setViewImagePost] = useState<string | null>(null);
-//   const [postContentType, setPostContentType] = useState<string>("");
-
-//   useEffect(() => {
-//     fetch("data.json")
-//       .then((res) => res.json())
-//       .then((data) => setPosts(data));
-//   }, []);
-
-//   const handleContentView = (postContent: string, contentType: string) => {
-//     setViewImagePost(postContent);
-//     setPostContentType(contentType);
-//   };
-
-//   useEffect(() => {
-//     if (viewImagePost) {
-//       document.body.classList.add("no-scroll");
-//     } else {
-//       document.body.classList.remove("no-scroll");
-//     }
-
-//     // Cleanup on component unmount
-//     return () => document.body.classList.remove("no-scroll");
-//   }, [viewImagePost]);
-
-//   console.log(viewImagePost);
-
-//   return (
-//     <div>
-//       {posts.map((post, idx) => (
-//         <div
-//           key={idx}
-//           className="mb-4 w-full mx-auto bg-white rounded-xl p-3 shadow"
-//         >
-//           {/* Header */}
-//           <div className="flex items-center">
-//             <div>
-//               <Image
-//                 alt="User DP"
-//                 src={post.profilePicture}
-//                 width={65}
-//                 height={65}
-//                 className="mt-1 rounded-full"
-//               ></Image>
-//             </div>
-//             <div className="flex-1 text-left px-2">
-//               <h1 className="font-semibold text-xl">{post.username}</h1>
-//               <span className="text-sm text-gray-400 flex gap items-center">
-//                 <IoLocationOutline className="text-lg" />
-//                 {post.location}
-//               </span>
-//             </div>
-//             <div>
-//               <HiDotsHorizontal className="text-[#07a1bc]/50 text-4xl cursor-pointer hover:bg-gray-100 px-1 py-1 rounded-xl" />
-//             </div>
-//           </div>
-
-//           {/* Content body */}
-//           <div className="mt-2 cursor-pointer">
-//             {post.contentType === "image" ? (
-//               <Image
-//                 alt="Post content"
-//                 src={post.postContent}
-//                 width={800}
-//                 height={600}
-//                 className="rounded-md h-[500px] object-cover hover:scale-105 custom-hover-img"
-//                 onClick={() =>
-//                   handleContentView(post.postContent, post.contentType)
-//                 }
-//               />
-//             ) : (
-//               <video
-//                 width="800"
-//                 height="500"
-//                 src={post.postContent}
-//                 controls
-//                 className="rounded-md h-[500px] bg-gray-950"
-//                 onClick={() =>
-//                   handleContentView(post.postContent, post.contentType)
-//                 }
-//               ></video>
-//             )}
-//           </div>
-
-//           {/* Footer */}
-//           <div className="mt-3">
-//             <p className="text-left text-lg">{post.caption}</p>
-
-//             <div className="flex mt-1 gap-3">
-//               {post?.hashtags.map((tag, idx) => (
-//                 <ul key={idx} className="text-[#07a1bc] font-light lowercase">
-//                   <li>{tag}</li>
-//                 </ul>
-//               ))}
-//             </div>
-//             <div className="text-end text-gray-400 text-sm font-light">
-//               <PostTimeConverter time={post.postedTime}></PostTimeConverter>
-//             </div>
-//           </div>
-//         </div>
-//       ))}
-
-//       {viewImagePost && (
-//         <ContentViewer
-//           postContentType={postContentType}
-//           imageURL={viewImagePost}
-//           onClose={() => setViewImagePost(null)}
-//         />
-//       )}
-//     </div>
-//   );
-// };
-
-// export default UserPost;
-
 'use client';
 import Image from 'next/legacy/image';
 import React, { useEffect, useState } from 'react';
@@ -147,6 +11,7 @@ import SkeletonLoader from '@/app/loading';
 import useLocalStorage from '@/hooks/useLocalStorage';
 import { getUserPost } from '@/api/posts/posts';
 import { IRefetchUserPostProp } from '@/app/(Pages)/home/page';
+import { getAccessToken } from '@/helpers/tokenStorage';
 
 interface Post {
   profilePicture: string;
@@ -177,7 +42,7 @@ const UserPost: React.FC<IRefetchUserPostProp> = ({
 }) => {
   const [posts, setPosts] = React.useState<Post[]>([]);
   const [viewImagePost, setViewImagePost] = useState<string | null>(null);
-  const [postContentType, setPostContentType] = useState<string>('');
+  // const [postContentType, setPostContentType] = useState<string>('');
   const [isLoading, setIsLoading] = useState(true);
   const { user, isAuthLoading } = useAuth();
   const { item: accessToken } = useLocalStorage('auth-token');
@@ -187,8 +52,8 @@ const UserPost: React.FC<IRefetchUserPostProp> = ({
       if (user) {
         try {
           setIsLoading(true);
-          let lsItem = accessToken && JSON.parse(accessToken).accessT;
-          const response = await getUserPost(lsItem, user?.userId);
+          // let lsItem = accessToken && JSON.parse(accessToken).accessT;
+          const response = await getUserPost(getAccessToken(), user?.userId);
           setPosts(response?.data?.data);
           // console.log(response?.data?.data);
         } catch (error) {
@@ -200,15 +65,15 @@ const UserPost: React.FC<IRefetchUserPostProp> = ({
     };
 
     fetchPosts();
-  }, [accessToken, user]);
+  }, [user]);
 
   useEffect(() => {
     const fetchPosts = async () => {
       if (user) {
         try {
           setIsLoading(true);
-          let lsItem = accessToken && JSON.parse(accessToken).accessT;
-          const response = await getUserPost(lsItem, user?.userId);
+          // let lsItem = accessToken && JSON.parse(accessToken).accessT;
+          const response = await getUserPost(getAccessToken(), user?.userId);
           setPosts(response?.data?.data);
           // console.log(response?.data?.data);
         } catch (error) {
