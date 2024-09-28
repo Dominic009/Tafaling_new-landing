f88ca9c05756a562d19edd1a16d9386deb8fc695
SUNIT <sunit.corneleous25@gmail.com>
2024-09-28 10:40:18 +0600

    user post fetch once

--- src/components/UserPost.tsx
+++ src/components/UserPost.tsx
@@ -1,17 +1,17 @@
-"use client";
-import Image from "next/legacy/image";
-import React, { useEffect, useState } from "react";
-import { HiDotsHorizontal } from "react-icons/hi";
-import { IoLocationOutline } from "react-icons/io5";
-import PostTimeConverter from "./PostTimeConverter";
-import ContentViewer from "./Content Viewer/ContentViewer";
-import { useAuth } from "@/context/AuthContext/AuthProvider";
-import ContentLoader from "./Loader/ContentLoader";
-import useLocalStorage from "@/hooks/useLocalStorage";
-import { getUserPost } from "@/api/posts/posts";
-import { IRefetchUserPostProp } from "@/app/(Pages)/home/page";
-import { getAccessToken } from "@/helpers/tokenStorage";
-import UserPostSkeleton from "./Loader/Skeleton/UserPostSkeleton";
+'use client';
+import Image from 'next/legacy/image';
+import React, { useEffect, useRef, useState } from 'react';
+import { HiDotsHorizontal } from 'react-icons/hi';
+import { IoLocationOutline } from 'react-icons/io5';
+import PostTimeConverter from './PostTimeConverter';
+import ContentViewer from './Content Viewer/ContentViewer';
+import { useAuth } from '@/context/AuthContext/AuthProvider';
+import ContentLoader from './Loader/ContentLoader';
+import useLocalStorage from '@/hooks/useLocalStorage';
+import { getUserPost } from '@/api/posts/posts';
+import { IRefetchUserPostProp } from '@/app/(Pages)/home/page';
+import { getAccessToken } from '@/helpers/tokenStorage';
+import UserPostSkeleton from './Loader/Skeleton/UserPostSkeleton';
 
 interface Post {
   profilePicture: string;
@@ -45,7 +45,8 @@ const UserPost: React.FC<IRefetchUserPostProp> = ({
   // const [postContentType, setPostContentType] = useState<string>('');
   const [isLoading, setIsLoading] = useState(true);
   const { user, isAuthLoading } = useAuth();
-  const { item: accessToken } = useLocalStorage("auth-token");
+  const { item: accessToken } = useLocalStorage('auth-token');
+  const isPostsFetched = useRef(false);
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -57,14 +58,17 @@ const UserPost: React.FC<IRefetchUserPostProp> = ({
           setPosts(response?.data?.data);
           // console.log(response?.data?.data);
         } catch (error) {
-          console.error("Error fetching posts:", error);
+          console.error('Error fetching posts:', error);
         } finally {
           setIsLoading(false);
         }
       }
     };
 
-    fetchPosts();
+    if (!isPostsFetched.current) {
+      fetchPosts();
+      isPostsFetched.current = true;
+    }
   }, [user]);
 
   useEffect(() => {
@@ -77,7 +81,7 @@ const UserPost: React.FC<IRefetchUserPostProp> = ({
           setPosts(response?.data?.data);
           // console.log(response?.data?.data);
         } catch (error) {
-          console.error("Error fetching posts:", error);
+          console.error('Error fetching posts:', error);
         } finally {
           setIsLoading(false);
           setRefetchUserPost && setRefetchUserPost(false);
@@ -97,13 +101,13 @@ const UserPost: React.FC<IRefetchUserPostProp> = ({
 
   useEffect(() => {
     if (viewImagePost) {
-      document.body.classList.add("no-scroll");
+      document.body.classList.add('no-scroll');
     } else {
-      document.body.classList.remove("no-scroll");
+      document.body.classList.remove('no-scroll');
     }
 
     // Cleanup on component unmount
-    return () => document.body.classList.remove("no-scroll");
+    return () => document.body.classList.remove('no-scroll');
   }, [viewImagePost]);
 
   // Setting the loading state to false
@@ -125,48 +129,48 @@ const UserPost: React.FC<IRefetchUserPostProp> = ({
             .map((post, idx) => (
               <div
                 key={idx}
-                className="mb-4 w-full mx-auto bg-white rounded-xl p-3 shadow"
+                className='mb-4 w-full mx-auto bg-white rounded-xl p-3 shadow'
               >
                 {/* Header */}
-                <div className="flex items-center">
+                <div className='flex items-center'>
                   <div>
                     <Image
-                      alt="User DP"
-                      src={user?.profile_picture || "/ProfileDP/Dummy.png"}
+                      alt='User DP'
+                      src={user?.profile_picture || '/ProfileDP/Dummy.png'}
                       // src={"/ProfileDP/Dummy.png"}
                       width={65}
                       height={65}
-                      className="mt-1 rounded-full"
+                      className='mt-1 rounded-full'
                     ></Image>
                   </div>
-                  <div className="flex-1 text-left px-2">
-                    <h1 className="font-semibold text-xl">
+                  <div className='flex-1 text-left px-2'>
+                    <h1 className='font-semibold text-xl'>
                       {post?.creator?.name}
                     </h1>
-                    <span className="text-sm text-gray-400 flex gap items-center">
-                      <IoLocationOutline className="text-lg" />
+                    <span className='text-sm text-gray-400 flex gap items-center'>
+                      <IoLocationOutline className='text-lg' />
                       {/* {post.location} */}
                       Location
                     </span>
                   </div>
                   <div>
-                    <HiDotsHorizontal className="text-[#07a1bc]/50 text-4xl cursor-pointer hover:bg-gray-100 px-1 py-1 rounded-xl" />
+                    <HiDotsHorizontal className='text-[#07a1bc]/50 text-4xl cursor-pointer hover:bg-gray-100 px-1 py-1 rounded-xl' />
                   </div>
                 </div>
 
                 {/* Content body */}
-                <div className="mt-2 cursor-pointer flex items-center justify-center">
+                <div className='mt-2 cursor-pointer flex items-center justify-center'>
                   {isLoading && <ContentLoader />}
                   {post.attachments[0]?.mimeType && (
                     <Image
-                      alt="Post content"
+                      alt='Post content'
                       src={`${post.attachments[0]?.fileURL}/${post.attachments[0]?.fileName}`}
                       width={800}
                       height={600}
-                      className="rounded-md h-[500px] object-cover hover:scale-105 custom-hover-img"
+                      className='rounded-md h-[500px] object-cover hover:scale-105 custom-hover-img'
                       onClick={() => handleContentView(post)}
                       onLoadingComplete={() => setIsLoading(false)}
-                      loading="lazy"
+                      loading='lazy'
                     />
                   )}
                   {/* {post.attachments[0]?.mimeType  ? (
@@ -196,23 +200,23 @@ const UserPost: React.FC<IRefetchUserPostProp> = ({
                 </div>
 
                 {/* Footer */}
-                <div className="mt-3">
-                  <p className="text-left text-lg">{post?.body}</p>
+                <div className='mt-3'>
+                  <p className='text-left text-lg'>{post?.body}</p>
 
-                  <div className="flex mt-1 gap-3">
+                  <div className='flex mt-1 gap-3'>
                     {/* {post?.hashtags?.map((tag, idx) => (
                 <ul key={idx} className='text-[#07a1bc] font-light lowercase'>
                   <li>{tag}</li>
                 </ul>
               ))} */}
-                    <ul className="text-[#07a1bc] font-light lowercase">
+                    <ul className='text-[#07a1bc] font-light lowercase'>
                       <li>#dummy</li>
                     </ul>
-                    <ul className="text-[#07a1bc] font-light lowercase">
+                    <ul className='text-[#07a1bc] font-light lowercase'>
                       <li>#dummy</li>
                     </ul>
                   </div>
-                  <div className="text-end text-gray-400 text-sm font-light">
+                  <div className='text-end text-gray-400 text-sm font-light'>
                     {/* <PostTimeConverter time={post?.postedTime}></PostTimeConverter> */}
                     <PostTimeConverter
                       time={post?.createdAt}
@@ -225,7 +229,7 @@ const UserPost: React.FC<IRefetchUserPostProp> = ({
           {viewImagePost && (
             <ContentViewer
               object={viewImagePost}
-              postContentType="image"
+              postContentType='image'
               onClose={() => setViewImagePost(null)}
             />
           )}
