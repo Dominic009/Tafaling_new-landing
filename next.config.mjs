b1303196d7ef0de54b7e3607287ee1a0692d6c4b
Sunit <sunit.corneleous25@gmail.com>
2024-10-10 15:57:29 +0600

    image config updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,37 @@
 /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   images: {
+//     remotePatterns: [
+//       'images.unsplash.com',
+//       'unsplash.com',
+//       'plus.unsplash.com',
+//       'videos.pexels.com',
+//       '99.237.86.169',
+//       '127.0.0.1',
+//     ],
+//   },
+// };
+
 const nextConfig = {
   images: {
-    domains: [
-      'images.unsplash.com',
-      'unsplash.com',
-      'plus.unsplash.com',
-      'videos.pexels.com',
-      '99.237.86.169',
-      '127.0.0.1',
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'videos.pexels.com',
+      },
+      {
+        protocol: 'http',
+        hostname: '99.237.86.169',
+        port: '7070',
+      },
     ],
   },
 };
