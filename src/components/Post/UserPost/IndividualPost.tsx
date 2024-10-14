46c23d1d5ba65e2250fac4873b2037074a798bc8
Sunit <sunit.corneleous25@gmail.com>
2024-10-14 20:16:20 +0600

    video height fixed

--- src/components/Post/UserPost/IndividualPost.tsx
+++ src/components/Post/UserPost/IndividualPost.tsx
@@ -1,22 +1,22 @@
-"use client";
+'use client';
 
-import PostTimeConverter from "@/components/PostTimeConverter";
-import { useAuth } from "@/context/AuthContext/AuthProvider";
-import Image from "next/image";
-import Link from "next/link";
-import { HiDotsHorizontal } from "react-icons/hi";
-import { IoLocationOutline } from "react-icons/io5";
-import { Post } from "./UserPost";
-import ContentLoader from "@/components/Loader/ContentLoader";
-import { useEffect, useState } from "react";
-import ContentViewer from "@/components/Content Viewer/ContentViewer";
-import { FaUserCircle } from "react-icons/fa";
-import DropDownMenu from "@/components/Drop down menu/DropDownMenu";
-import Modal from "@/components/Modal/Modal";
-import ChangePrivacy from "../ChangePrivacy/ChangePrivacy";
-import { FaEye } from "react-icons/fa6";
-import LinkPreview from "../LinkPreview/LinkPreview";
-import Interaction from "@/components/User interactions/Interaction";
+import PostTimeConverter from '@/components/PostTimeConverter';
+import { useAuth } from '@/context/AuthContext/AuthProvider';
+import Image from 'next/image';
+import Link from 'next/link';
+import { HiDotsHorizontal } from 'react-icons/hi';
+import { IoLocationOutline } from 'react-icons/io5';
+import { Post } from './UserPost';
+import ContentLoader from '@/components/Loader/ContentLoader';
+import { useEffect, useState } from 'react';
+import ContentViewer from '@/components/Content Viewer/ContentViewer';
+import { FaUserCircle } from 'react-icons/fa';
+import DropDownMenu from '@/components/Drop down menu/DropDownMenu';
+import Modal from '@/components/Modal/Modal';
+import ChangePrivacy from '../ChangePrivacy/ChangePrivacy';
+import { FaEye } from 'react-icons/fa6';
+import LinkPreview from '../LinkPreview/LinkPreview';
+import Interaction from '@/components/User interactions/Interaction';
 
 interface IPostProps {
   post: Post;
@@ -45,8 +45,8 @@ const IndividualPost: React.FC<IPostProps> = ({
     // console.log(object);
   };
 
-  const [text, setText] = useState("");
-  const [links, setLinks] = useState<string | "">("");
+  const [text, setText] = useState('');
+  const [links, setLinks] = useState<string | ''>('');
 
   useEffect(() => {
     const urlRegex = /(https?:\/\/[^\s]+)/g;
@@ -55,18 +55,18 @@ const IndividualPost: React.FC<IPostProps> = ({
     if (detectedLinks) {
       setLinks(text);
     } else {
-      setLinks("");
+      setLinks('');
     }
   }, [text]);
 
   useEffect(() => {
-    if (post.body.includes("http")) {
-      const indexOfHttpStart = post.body.indexOf("http");
+    if (post.body.includes('http')) {
+      const indexOfHttpStart = post.body.indexOf('http');
       const httpText = post.body.slice(indexOfHttpStart);
 
       //handle white space after link
-      if (httpText.includes(" ")) {
-        const onlyHttpLink = httpText.split(" ")[0];
+      if (httpText.includes(' ')) {
+        const onlyHttpLink = httpText.split(' ')[0];
         setText(onlyHttpLink);
       } else {
         setText(httpText);
@@ -76,18 +76,18 @@ const IndividualPost: React.FC<IPostProps> = ({
 
   useEffect(() => {
     if (viewImagePost) {
-      document.body.classList.add("no-scroll");
+      document.body.classList.add('no-scroll');
     } else {
-      document.body.classList.remove("no-scroll");
+      document.body.classList.remove('no-scroll');
     }
 
     // Cleanup on component unmount
-    return () => document.body.classList.remove("no-scroll");
+    return () => document.body.classList.remove('no-scroll');
   }, [viewImagePost]);
 
   // Post selected privacy
   const userPirvacyText = user?.userPrivacy?.find(
-    (item) => item.privacy_setting_id === post.privacyId
+    item => item.privacy_setting_id === post.privacyId
   );
   const urlRegex = /(https?:\/\/[^\s]+)/g;
 
@@ -99,9 +99,9 @@ const IndividualPost: React.FC<IPostProps> = ({
           <a
             key={index}
             href={part}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-blue-500"
+            target='_blank'
+            rel='noopener noreferrer'
+            className='text-blue-500'
           >
             {part}
           </a>
@@ -116,7 +116,7 @@ const IndividualPost: React.FC<IPostProps> = ({
     const fullText = post.body;
 
     if (!isPostExpanded && post.body.length > textLimit) {
-      return createClickableLinks(shortText + "...");
+      return createClickableLinks(shortText + '...');
     } else if (isPostExpanded) {
       return createClickableLinks(fullText);
     } else {
@@ -127,49 +127,49 @@ const IndividualPost: React.FC<IPostProps> = ({
   return (
     <div
       key={key}
-      className="mb-4 w-full mx-auto bg-white rounded-xl p-3 shadow"
+      className='mb-4 w-full mx-auto bg-white rounded-xl p-3 shadow'
     >
       {/* Header */}
-      <div className="flex items-center mb-3">
-        <div className="w-16 h-16 rounded-full flex items-center justify-center">
-          <Link href={"/user-profile"}>
+      <div className='flex items-center mb-3'>
+        <div className='w-16 h-16 rounded-full flex items-center justify-center'>
+          <Link href={'/user-profile'}>
             <Image
-              alt="User DP"
-              src={user?.profile_picture || "/ProfileDP/Dummy.png"}
+              alt='User DP'
+              src={user?.profile_picture || '/ProfileDP/Dummy.png'}
               width={50}
               height={50}
-              objectFit="cover"
-              className="w-16 h-16 rounded-full"
+              objectFit='cover'
+              className='w-16 h-16 rounded-full'
             ></Image>
           </Link>
         </div>
-        <div className="flex-1 text-left px-2">
-          <Link href={"/user-profile"}>
-            <h1 className="font-semibold text-xl">{post?.creator?.name}</h1>
+        <div className='flex-1 text-left px-2'>
+          <Link href={'/user-profile'}>
+            <h1 className='font-semibold text-xl'>{post?.creator?.name}</h1>
           </Link>
-          <div className="flex items-center gap-3">
-            <h5 className="text-sm text-gray-400 flex gap items-center">
-              <IoLocationOutline className="text-lg" />
+          <div className='flex items-center gap-3'>
+            <h5 className='text-sm text-gray-400 flex gap items-center'>
+              <IoLocationOutline className='text-lg' />
               {/* {post.location} */}
               Location
             </h5>
-            <span className="w-1 h-1 rounded-full bg-[#d4d4d4]"></span>
-            <h5 className="text-sm text-gray-400 flex gap items-center">
-              <p className="flex items-center gap-1">
-                <FaEye className="inline-block" />{" "}
+            <span className='w-1 h-1 rounded-full bg-[#d4d4d4]'></span>
+            <h5 className='text-sm text-gray-400 flex gap items-center'>
+              <p className='flex items-center gap-1'>
+                <FaEye className='inline-block' />{' '}
                 {userPirvacyText?.privacy_setting_name}
               </p>
             </h5>
           </div>
         </div>
         {user?.user_name && (
-          <div className="relative">
+          <div className='relative'>
             <HiDotsHorizontal
               onClick={() => setToggleEditPost(!toggleEditPost)}
-              className="text-[#07a1bc]/50 text-4xl cursor-pointer hover:bg-gray-100 px-1 py-1 rounded-xl"
+              className='text-[#07a1bc]/50 text-4xl cursor-pointer hover:bg-gray-100 px-1 py-1 rounded-xl'
             />
             {toggleEditPost && (
-              <DropDownMenu bg="[#f4f7f8]" top="6" right="3">
+              <DropDownMenu bg='[#f4f7f8]' top='6' right='3'>
                 {/* dropdown buttons here */}
                 {/* <button
             
@@ -184,11 +184,11 @@ const IndividualPost: React.FC<IPostProps> = ({
               </span>
             </button> */}
                 <button
-                  className="hover:bg-[#dfdfdf] p-1 rounded-md cursor-pointer transition-colors ease-linear"
+                  className='hover:bg-[#dfdfdf] p-1 rounded-md cursor-pointer transition-colors ease-linear'
                   onClick={() => setEditPrivacyModal(true)}
                 >
-                  <span className="flex items-center gap-2 text-gray-800">
-                    <FaUserCircle className="text-xl text-[#00B4DB]" /> Edit
+                  <span className='flex items-center gap-2 text-gray-800'>
+                    <FaUserCircle className='text-xl text-[#00B4DB]' /> Edit
                     Privacy
                   </span>
                 </button>
@@ -199,42 +199,42 @@ const IndividualPost: React.FC<IPostProps> = ({
       </div>
 
       {/* Content body */}
-      <div className="mt-2 cursor-pointer flex items-center justify-center overflow-hidden hover:drop-shadow-xl rounded-md custom-hover">
+      <div className='mt-2 cursor-pointer flex items-center justify-center overflow-hidden hover:drop-shadow-xl rounded-md custom-hover'>
         {isLoading && <ContentLoader />}
 
-        {post.attachments[0]?.mimeType.includes("image") && (
+        {post.attachments[0]?.mimeType.includes('image') && (
           <Image
-            alt="Post content"
+            alt='Post content'
             src={`${post.attachments[0]?.fileURL}/${post.attachments[0]?.fileName}`}
             width={800}
             height={500}
-            className="rounded-md object-cover hover:scale-[102%] custom-hover-img h-[500px]"
+            className='rounded-md object-cover hover:scale-[102%] custom-hover-img h-[500px]'
             onClick={() => handleContentView(post)}
             onLoadingComplete={() => setIsLoading(false)}
-            loading="lazy"
+            loading='lazy'
           />
         )}
-        {post.attachments[0]?.mimeType.includes("video") && (
+        {post.attachments[0]?.mimeType.includes('video') && (
           <video
-            width="800"
-            height="500"
+            width='800'
+            height='500'
             controls
-            className="rounded-md h-[500px]"
+            className='rounded-md h-[353px]'
             onClick={() => handleContentView(post)}
             onCanPlay={() => setIsLoading(false)}
             autoPlay={false}
             src={`${post.attachments[0]?.fileURL}/${post.attachments[0]?.fileName}`}
           >
-            <source src={post.postContent} type="video/mp4" />
+            <source src={post.postContent} type='video/mp4' />
             Your browser does not support the video tag.
           </video>
         )}
       </div>
 
       {/* Footer */}
-      <div className="mt-3">
+      <div className='mt-3'>
         {/* <p className='text-left text-lg text-wrap'>{post?.body}</p> */}
-        <p className="text-left text-lg text-wrap">
+        <p className='text-left text-lg text-wrap'>
           {renderPostBody()}
           {/* {post.body.length > 90 ? `${post.body.slice(0, 90)}...`} */}
           {!isPostExpanded &&
@@ -249,7 +249,7 @@ const IndividualPost: React.FC<IPostProps> = ({
           {post.body.length > textLimit && !isPostExpanded && (
             <small
               onClick={() => setIsPostExpanded(!isPostExpanded)}
-              className="text-gray-400 cursor-pointer"
+              className='text-gray-400 cursor-pointer'
             >
               See more
             </small>
@@ -257,14 +257,14 @@ const IndividualPost: React.FC<IPostProps> = ({
           {isPostExpanded && (
             <small
               onClick={() => setIsPostExpanded(!isPostExpanded)}
-              className="text-gray-400 cursor-pointer"
+              className='text-gray-400 cursor-pointer'
             >
               ..Hide
             </small>
           )}
         </p>
 
-        <div className="flex mt-1 gap-3">
+        <div className='flex mt-1 gap-3'>
           {links && (
             <LinkPreview
               forPost={true}
@@ -286,12 +286,12 @@ const IndividualPost: React.FC<IPostProps> = ({
           </ul> */}
         </div>
 
-        <div className="mt-3">
+        <div className='mt-3'>
           <Interaction />
         </div>
 
         {/* post created time */}
-        <div className="text-end text-gray-400 text-sm font-light">
+        <div className='text-end text-gray-400 text-sm font-light'>
           {/* <PostTimeConverter time={post?.postedTime}></PostTimeConverter> */}
           <PostTimeConverter time={post?.createdAt}></PostTimeConverter>
         </div>
@@ -300,7 +300,7 @@ const IndividualPost: React.FC<IPostProps> = ({
       {viewImagePost && (
         <ContentViewer
           object={viewImagePost}
-          postContentType="image"
+          postContentType='image'
           onClose={() => setViewImagePost(null)}
         />
       )}
@@ -308,7 +308,7 @@ const IndividualPost: React.FC<IPostProps> = ({
       <Modal
         isOpen={editPrivacyModal}
         onClose={() => setEditPrivacyModal(!editPrivacyModal)}
-        width={"40%"}
+        width={'40%'}
       >
         <h1>Edit Privacy</h1>
         <ChangePrivacy
