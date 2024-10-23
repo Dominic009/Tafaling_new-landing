0e392fa24c7cc1ec02580b646ef54612a11aafe0
Dominic009 <dpg11000@gmail.com>
2024-10-23 11:30:22 +0600

    Theme config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,17 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      // colors: {
+      //   background: '' ,
+      //   primary: '',
+      //   secondary: '',
+      // }
+      colors: {
+        primary: "var(--primary-color)",
+        secondary: "var(--secondary-color)",
+        background: "var(--background-color)",
+        text: "var(--text-color)",
+      },
     },
   },
   plugins: [],
