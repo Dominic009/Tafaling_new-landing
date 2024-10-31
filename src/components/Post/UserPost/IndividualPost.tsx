4fb6cf77fe6f8aac770e19b761c63954decbb63f
Sunit Corneleous <sunit.corneleous25@gmail.com>
2024-10-31 13:09:33 +0600

    no-scroll not fixed for view content modal

--- src/components/Post/UserPost/IndividualPost.tsx
+++ src/components/Post/UserPost/IndividualPost.tsx
@@ -86,11 +86,11 @@ const IndividualPost: React.FC<IPostProps> = ({
 
   useEffect(() => {
     if (viewImagePost) {
-      setScrollPosition(window.scrollY);
+      // setScrollPosition(window.scrollY);
       document.body.classList.add('no-scroll');
     } else {
       document.body.classList.remove('no-scroll');
-      window.scrollTo(0, scrollPosition);
+      // window.scrollTo(0, scrollPosition);
     }
 
     // Cleanup on component unmount
@@ -133,53 +133,54 @@ const IndividualPost: React.FC<IPostProps> = ({
   };
 
   return (
-    <div
-      key={key}
-      className='mb-4 w-full mx-auto bg-white rounded-xl p-3 shadow'
-    >
-      {/* Header */}
-      <div className='flex items-center mb-3'>
-        <div className='w-16 h-16 rounded-full flex items-center justify-center'>
-          <Link href={`/user-profile/${user?.userId}`}>
-            <Image
-              alt='User DP'
-              src={user?.profile_picture || '/ProfileDP/Dummy.png'}
-              width={50}
-              height={50}
-              objectFit='cover'
-              className='w-16 h-16 rounded-full'
-            ></Image>
-          </Link>
-        </div>
-        <div className='flex-1 text-left px-2'>
-          <Link href={`/user-profile/${user?.userId}`}>
-            <h1 className='font-semibold text-xl'>{post?.creator?.name}</h1>
-          </Link>
-          <div className='flex items-center gap-3'>
-            <h5 className='text-sm text-gray-400 flex gap items-center'>
-              <IoLocationOutline className='text-lg' />
-              {/* {post.location} */}
-              Location
-            </h5>
-            <span className='w-1 h-1 rounded-full bg-[#d4d4d4]'></span>
-            <h5 className='text-sm text-gray-400 flex gap items-center'>
-              <p className='flex items-center gap-1'>
-                <FaEye className='inline-block' />{' '}
-                {postPrivacy?.privacy_setting_name}
-              </p>
-            </h5>
+    <>
+      <div
+        key={key}
+        className='mb-4 w-full mx-auto bg-white rounded-xl p-3 shadow'
+      >
+        {/* Header */}
+        <div className='flex items-center mb-3'>
+          <div className='w-16 h-16 rounded-full flex items-center justify-center'>
+            <Link href={`/user-profile/${user?.userId}`}>
+              <Image
+                alt='User DP'
+                src={user?.profile_picture || '/ProfileDP/Dummy.png'}
+                width={50}
+                height={50}
+                objectFit='cover'
+                className='w-16 h-16 rounded-full'
+              ></Image>
+            </Link>
           </div>
-        </div>
-        {user?.user_name && (
-          <div className='relative z-40'>
-            <HiDotsHorizontal
-              onClick={() => setToggleEditPost(!toggleEditPost)}
-              className='text-[#07a1bc]/50 text-4xl cursor-pointer hover:bg-gray-100 px-1 py-1 rounded-xl'
-            />
-            {toggleEditPost && (
-              <DropDownMenu bg='[#f4f7f8]' top='6' right='3'>
-                {/* dropdown buttons here */}
-                {/* <button
+          <div className='flex-1 text-left px-2'>
+            <Link href={`/user-profile/${user?.userId}`}>
+              <h1 className='font-semibold text-xl'>{post?.creator?.name}</h1>
+            </Link>
+            <div className='flex items-center gap-3'>
+              <h5 className='text-sm text-gray-400 flex gap items-center'>
+                <IoLocationOutline className='text-lg' />
+                {/* {post.location} */}
+                Location
+              </h5>
+              <span className='w-1 h-1 rounded-full bg-[#d4d4d4]'></span>
+              <h5 className='text-sm text-gray-400 flex gap items-center'>
+                <p className='flex items-center gap-1'>
+                  <FaEye className='inline-block' />{' '}
+                  {postPrivacy?.privacy_setting_name}
+                </p>
+              </h5>
+            </div>
+          </div>
+          {user?.user_name && (
+            <div className='relative z-40'>
+              <HiDotsHorizontal
+                onClick={() => setToggleEditPost(!toggleEditPost)}
+                className='text-[#07a1bc]/50 text-4xl cursor-pointer hover:bg-gray-100 px-1 py-1 rounded-xl'
+              />
+              {toggleEditPost && (
+                <DropDownMenu bg='[#f4f7f8]' top='6' right='3'>
+                  {/* dropdown buttons here */}
+                  {/* <button
             
             onClick={() => {
               setSelectedPost(post);  
@@ -191,132 +192,152 @@ const IndividualPost: React.FC<IPostProps> = ({
                 <FaEdit className="text-xl text-[#00B4DB]" /> Edit Post
               </span>
             </button> */}
-                <button
-                  className='hover:bg-[#dfdfdf] p-1 rounded-md cursor-pointer transition-colors ease-linear'
-                  onClick={() => setEditPrivacyModal(true)}
-                >
-                  <span className='flex items-center gap-2 text-gray-800'>
-                    <FaUserCircle className='text-xl text-[#00B4DB]' /> Edit
-                    Privacy
-                  </span>
-                </button>
+                  <button
+                    className='hover:bg-[#dfdfdf] p-1 rounded-md cursor-pointer transition-colors ease-linear'
+                    onClick={() => setEditPrivacyModal(true)}
+                  >
+                    <span className='flex items-center gap-2 text-gray-800'>
+                      <FaUserCircle className='text-xl text-[#00B4DB]' /> Edit
+                      Privacy
+                    </span>
+                  </button>
 
-                {/* Delete post */}
-                <button
-                  className='hover:bg-[#dfdfdf] p-1 rounded-md cursor-pointer transition-colors ease-linear'
-                  onClick={() => setEditPrivacyModal(true)}
-                >
-                  <span className='flex items-center gap-2 text-gray-800'>
-                    <MdDeleteForever className='text-xl text-[#dc2626]' />{' '}
-                    Delete Post
-                  </span>
-                </button>
-              </DropDownMenu>
-            )}
-          </div>
-        )}
-      </div>
-
-      {/* Content body */}
-      <div className='mt-2 cursor-pointer flex items-center justify-center overflow-hidden hover:drop-shadow-xl rounded-md custom-hover'>
-        {isLoading && <ContentLoader />}
-
-        {post.attachments[0]?.mimeType.includes('image') && (
-          <Image
-            alt='Post content'
-            src={`${post.attachments[0]?.fileURL}/${post.attachments[0]?.fileName}`}
-            width={800}
-            height={500}
-            className={`rounded-md object-cover hover:scale-[102%] custom-hover-img h-[500px]`}
-            onClick={() => handleContentView(post)}
-            onLoadingComplete={() => setIsLoading && setIsLoading(false)}
-            loading='lazy'
-          />
-        )}
-        {post.attachments[0]?.mimeType.includes('video') && (
-          <video
-            width='800'
-            height='500'
-            controls
-            className='rounded-md h-[353px]'
-            onClick={() => handleContentView(post)}
-            onCanPlay={() => setIsLoading && setIsLoading(false)}
-            autoPlay={false}
-            src={`${post.attachments[0]?.fileURL}/${post.attachments[0]?.fileName}`}
-          >
-            <source src={post.postContent} type='video/mp4' />
-            Your browser does not support the video tag.
-          </video>
-        )}
-      </div>
-
-      {/* Footer */}
-      <div className='mt-3'>
-        {/* <p className='text-left text-lg text-wrap'>{post?.body}</p> */}
-        <p className='text-left text-lg text-wrap'>
-          {renderPostBody()}
-          {/* {post.body.length > 90 ? `${post.body.slice(0, 90)}...`} */}
-          {!isPostExpanded &&
-            post.body.length > textLimit &&
-            `${post.body.slice(0, textLimit)}...`}
-
-          {isPostExpanded && post.body.length > textLimit && `${post.body}`}
-
-          {/* {post.body.length < textLimit && `${post.body}`} */}
-
-          {/* Show 'Read More' only if the text exceeds the limit and is not expanded */}
-          {post.body.length > textLimit && !isPostExpanded && (
-            <small
-              onClick={() => setIsPostExpanded(!isPostExpanded)}
-              className='text-gray-400 cursor-pointer'
-            >
-              See more
-            </small>
+                  {/* Delete post */}
+                  <button
+                    className='hover:bg-[#dfdfdf] p-1 rounded-md cursor-pointer transition-colors ease-linear'
+                    onClick={() => setEditPrivacyModal(true)}
+                  >
+                    <span className='flex items-center gap-2 text-gray-800'>
+                      <MdDeleteForever className='text-xl text-[#dc2626]' />{' '}
+                      Delete Post
+                    </span>
+                  </button>
+                </DropDownMenu>
+              )}
+            </div>
           )}
-          {isPostExpanded && (
-            <small
-              onClick={() => setIsPostExpanded(!isPostExpanded)}
-              className='text-gray-400 cursor-pointer'
-            >
-              ..Hide
-            </small>
-          )}
-        </p>
+        </div>
 
-        <div className='flex mt-1 gap-3'>
-          {links && (
-            <LinkPreview
-              forPost={true}
-              url={links}
-              closeLinkPreview={setLinks}
-              disableCloseButton={true}
-              post={post}
+        {/* Content body */}
+        <div className='mt-2 cursor-pointer flex items-center justify-center overflow-hidden hover:drop-shadow-xl rounded-md custom-hover'>
+          {isLoading && <ContentLoader />}
+
+          {post.attachments[0]?.mimeType.includes('image') && (
+            <Image
+              alt='Post content'
+              src={`${post.attachments[0]?.fileURL}/${post.attachments[0]?.fileName}`}
+              width={800}
+              height={500}
+              className={`rounded-md object-cover hover:scale-[102%] custom-hover-img h-[500px]`}
+              onClick={() => handleContentView(post)}
+              onLoadingComplete={() => setIsLoading && setIsLoading(false)}
+              loading='lazy'
             />
           )}
-          {/* {post?.hashtags?.map((tag, idx) => (
+          {post.attachments[0]?.mimeType.includes('video') && (
+            <video
+              width='800'
+              height='500'
+              controls
+              className='rounded-md h-[353px]'
+              onClick={() => handleContentView(post)}
+              onCanPlay={() => setIsLoading && setIsLoading(false)}
+              autoPlay={false}
+              src={`${post.attachments[0]?.fileURL}/${post.attachments[0]?.fileName}`}
+            >
+              <source src={post.postContent} type='video/mp4' />
+              Your browser does not support the video tag.
+            </video>
+          )}
+        </div>
+
+        {/* Footer */}
+        <div className='mt-3'>
+          {/* <p className='text-left text-lg text-wrap'>{post?.body}</p> */}
+          <p className='text-left text-lg text-wrap'>
+            {renderPostBody()}
+            {/* {post.body.length > 90 ? `${post.body.slice(0, 90)}...`} */}
+            {!isPostExpanded &&
+              post.body.length > textLimit &&
+              `${post.body.slice(0, textLimit)}...`}
+
+            {isPostExpanded && post.body.length > textLimit && `${post.body}`}
+
+            {/* {post.body.length < textLimit && `${post.body}`} */}
+
+            {/* Show 'Read More' only if the text exceeds the limit and is not expanded */}
+            {post.body.length > textLimit && !isPostExpanded && (
+              <small
+                onClick={() => setIsPostExpanded(!isPostExpanded)}
+                className='text-gray-400 cursor-pointer'
+              >
+                See more
+              </small>
+            )}
+            {isPostExpanded && (
+              <small
+                onClick={() => setIsPostExpanded(!isPostExpanded)}
+                className='text-gray-400 cursor-pointer'
+              >
+                ..Hide
+              </small>
+            )}
+          </p>
+
+          <div className='flex mt-1 gap-3'>
+            {links && (
+              <LinkPreview
+                forPost={true}
+                url={links}
+                closeLinkPreview={setLinks}
+                disableCloseButton={true}
+                post={post}
+              />
+            )}
+            {/* {post?.hashtags?.map((tag, idx) => (
             <ul key={idx} className='text-[#07a1bc] font-light lowercase'>
                 <li>{tag}</li>
             </ul>
             ))} */}
-          {/* <ul className='text-[#07a1bc] font-light lowercase'>
+            {/* <ul className='text-[#07a1bc] font-light lowercase'>
                 <li>#dummy</li>
             </ul>
           <ul className='text-[#07a1bc] font-light lowercase'>
             <li>#dummy</li>
           </ul> */}
+          </div>
+
+          <div className='mt-3'>
+            <Interaction post={post} />
+          </div>
+
+          {/* post created time */}
+          <div className='text-end text-gray-400 text-sm font-light'>
+            {/* <PostTimeConverter time={post?.postedTime}></PostTimeConverter> */}
+            <PostTimeConverter time={post?.createdAt}></PostTimeConverter>
+          </div>
         </div>
 
-        <div className='mt-3'>
-          <Interaction post={post} />
-        </div>
-
-        {/* post created time */}
-        <div className='text-end text-gray-400 text-sm font-light'>
-          {/* <PostTimeConverter time={post?.postedTime}></PostTimeConverter> */}
-          <PostTimeConverter time={post?.createdAt}></PostTimeConverter>
-        </div>
+        <Modal
+          isOpen={editPrivacyModal}
+          onClose={() => setEditPrivacyModal(!editPrivacyModal)}
+          width={'40%'}
+        >
+          <h1>Edit Privacy</h1>
+          <ChangePrivacy
+            modal={editPrivacyModal}
+            setModal={setEditPrivacyModal}
+            setRefetchUserPost={setRefetchUserPost}
+            userPrivacy={user?.userPrivacy && user?.userPrivacy}
+            postData={{
+              postId: post.postId,
+              privacyId: post.privacyId,
+            }}
+            setToggleEditPost={setToggleEditPost}
+            setPostPrivacy={setPostPrivacy}
+          />
+        </Modal>
       </div>
-
       {viewImagePost && (
         <ContentViewer
           object={viewImagePost}
@@ -324,27 +345,7 @@ const IndividualPost: React.FC<IPostProps> = ({
           onClose={() => setViewImagePost(null)}
         />
       )}
-
-      <Modal
-        isOpen={editPrivacyModal}
-        onClose={() => setEditPrivacyModal(!editPrivacyModal)}
-        width={'40%'}
-      >
-        <h1>Edit Privacy</h1>
-        <ChangePrivacy
-          modal={editPrivacyModal}
-          setModal={setEditPrivacyModal}
-          setRefetchUserPost={setRefetchUserPost}
-          userPrivacy={user?.userPrivacy && user?.userPrivacy}
-          postData={{
-            postId: post.postId,
-            privacyId: post.privacyId,
-          }}
-          setToggleEditPost={setToggleEditPost}
-          setPostPrivacy={setPostPrivacy}
-        />
-      </Modal>
-    </div>
+    </>
   );
 };
 
