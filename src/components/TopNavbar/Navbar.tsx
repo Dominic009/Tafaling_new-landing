d521840010e7e59a443eb7af1eb6c12f0401ee03
Dominic009 <dpg11000@gmail.com>
2024-11-06 14:54:07 +0600

    Fixings

--- src/components/TopNavbar/Navbar.tsx
+++ src/components/TopNavbar/Navbar.tsx
@@ -1,21 +1,21 @@
-'use client';
-import Image from 'next/legacy/image';
-import React, { useEffect, useState } from 'react';
-import { TiHome } from 'react-icons/ti';
-import { BsFillPeopleFill } from 'react-icons/bs';
-import { HiOutlineSearch } from 'react-icons/hi';
-import Link from 'next/link';
-import { usePathname } from 'next/navigation';
-import { useAuth } from '@/context/AuthContext/AuthProvider';
-import 'animate.css';
-import { FaUserCircle } from 'react-icons/fa';
-import AuthUserNavMenu from './AuthUserNavMenu';
-import PrimaryBtn from '../PrimaryBtn';
-import { IoMdLogIn } from 'react-icons/io';
-import SearchInput from '../Search Input/SearchInput';
-import { searchUser } from '@/api/user/user';
-import { getAccessToken } from '@/helpers/tokenStorage';
-import IndividualSearchUser from './UserSearch/IndividualSearchUser';
+"use client";
+import Image from "next/legacy/image";
+import React, { useEffect, useState } from "react";
+import { TiHome } from "react-icons/ti";
+import { BsFillPeopleFill } from "react-icons/bs";
+import { HiOutlineSearch } from "react-icons/hi";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { useAuth } from "@/context/AuthContext/AuthProvider";
+import "animate.css";
+import { FaUserCircle } from "react-icons/fa";
+import AuthUserNavMenu from "./AuthUserNavMenu";
+import PrimaryBtn from "../PrimaryBtn";
+import { IoMdLogIn } from "react-icons/io";
+import SearchInput from "../Search Input/SearchInput";
+import { searchUser } from "@/api/user/user";
+import { getAccessToken } from "@/helpers/tokenStorage";
+import IndividualSearchUser from "./UserSearch/IndividualSearchUser";
 
 export interface ISearchUser {
   userId: number;
@@ -44,10 +44,10 @@ const Navbar: React.FC = () => {
     useState<boolean>(false);
 
   //filters for user search
-  const [inputValue, setInputValue] = useState('');
+  const [inputValue, setInputValue] = useState("");
 
   useEffect(() => {
-    if (inputValue === '') {
+    if (inputValue === "") {
       setIsSearchLoading(false);
       setIsShowingSearchResults(false);
     }
@@ -58,41 +58,41 @@ const Navbar: React.FC = () => {
 
   const routes = [
     {
-      name: 'Home',
-      path: '/home',
+      name: "Home",
+      path: "/home",
       icon: (
         <TiHome
-          title='Home'
-          className='text-3xl  hover:text-white custom-hover'
+          title="Home"
+          className="text-3xl  hover:text-white custom-hover"
         />
       ),
     },
     {
-      name: 'Profile',
+      name: "Profile",
       path: `/user-profile/${user?.userId}`,
       icon: (
         <FaUserCircle
-          title='Profile'
-          className='text-2xl mb-1 hover:text-white custom-hover'
+          title="Profile"
+          className="text-2xl mb-1 hover:text-white custom-hover"
         />
       ),
     },
     {
-      name: 'Requests',
-      path: '/requests',
+      name: "Requests",
+      path: "/requests",
       icon: (
         <BsFillPeopleFill
-          title='Requests'
-          className='text-2xl  hover:text-white custom-hover'
+          title="Requests"
+          className="text-2xl  hover:text-white custom-hover"
         />
       ),
     },
   ];
 
   if (
-    currentPath === '/login' ||
-    currentPath === '/register' ||
-    currentPath === '/verifyEmail'
+    currentPath === "/login" ||
+    currentPath === "/register" ||
+    currentPath === "/verifyEmail"
   ) {
     return null; // Do not render the Navbar on these paths
   }
@@ -122,23 +122,23 @@ const Navbar: React.FC = () => {
   };
 
   return (
-    <nav className='h-[70px] grid grid-cols-2 md:grid-cols-3 gap-5 bg-gradient-to-r from-secondary to-[#012349] items-center px-5 w-full custom-hover'>
+    <nav className="h-[70px] grid grid-cols-2 md:grid-cols-3 gap-5 bg-gradient-to-r from-secondary to-[#012349] items-center px-5 w-full custom-hover">
       {/* Left Section */}
       <div>
-        <Link href={'/home'}>
+        <Link href={"/home"}>
           <Image
-            src={'/Tafaling logo.png'}
+            src={"/Tafaling logo.png"}
             width={130}
             height={55}
-            alt='Brand logo'
+            alt="Brand logo"
           ></Image>
         </Link>
       </div>
 
       {/* Middle Section Navlinks*/}
-      <div className='hidden md:block'>
-        <div className='flex gap-9 items-center justify-center'>
-          {routes.map(path => {
+      <div className="hidden md:block">
+        <div className="flex gap-9 items-center justify-center">
+          {routes.map((path) => {
             const isActive = path.path === currentPath;
             return (
               <Link
@@ -146,8 +146,8 @@ const Navbar: React.FC = () => {
                 key={path.name}
                 className={`${
                   isActive
-                    ? 'text-white border-b-2 border-[#42C6DE]'
-                    : 'text-white/50'
+                    ? "text-white border-b-2 border-[#42C6DE]"
+                    : "text-white/50"
                 }`}
               >
                 <span>{path.icon}</span>
@@ -158,28 +158,26 @@ const Navbar: React.FC = () => {
       </div>
 
       {/* Right Section */}
-      <div className={`relative ${user ? 'flex justify-end' : 'grid-cols-2'}`}>
+      <div className={`relative ${user ? "flex justify-end" : "grid-cols-2"}`}>
         <div
           className={`grid grid-cols-2 lg:grid-cols-4 ${
-            !user && 'flex'
+            !user && "flex"
           } items-center gap-5`}
         >
           {/* Search field */}
-          <div className='hidden md:block relative lg:col-span-3'>
-            {user && (
-              <SearchInput
-                setInputValue={setInputValue}
-                inputValue={inputValue}
-                searchUserHandler={searchUserHandler}
-                user={searchedUsers}
-                setSearchedUsers={setSearchedUsers}
-                setIsShowingSearchResults={setIsShowingSearchResults}
-              />
-            )}
+          <div className="hidden md:block relative lg:col-span-3">
+            <SearchInput
+              setInputValue={setInputValue}
+              inputValue={inputValue}
+              searchUserHandler={searchUserHandler}
+              user={searchedUsers}
+              setSearchedUsers={setSearchedUsers}
+              setIsShowingSearchResults={setIsShowingSearchResults}
+            />
 
-            <div className='absolute top-12 -right-5 flex flex-col'>
+            <div className="absolute top-12 -right-5 flex flex-col">
               {isShowingSearchResults && (
-                <div className='bg-gray-200/90 backdrop-blur-lg rounded-lg py-3 text-center w-[400px]'>
+                <div className="bg-gray-200/90 backdrop-blur-lg rounded-lg py-3 text-center w-[400px]">
                   {searchedUsers.map((item, i) => (
                     <IndividualSearchUser
                       key={i}
@@ -190,17 +188,17 @@ const Navbar: React.FC = () => {
                   ))}
 
                   {isSearchLoading && (
-                    <div className='z-50 flex'>
-                      <div className='grid lg:grid-cols-6 gap-1 items-center justify-center mb-2 py-1 px-3 bg-gray-50 rounded-lg drop-shadow w-[90%] mx-auto scale-75 animate-pulse'>
-                        <div className='w-12 h-12 rounded-full bg-gray-300 flex items-center justify-center'></div>
+                    <div className="z-50 flex">
+                      <div className="grid lg:grid-cols-6 gap-1 items-center justify-center mb-2 py-1 px-3 bg-gray-50 rounded-lg drop-shadow w-[90%] mx-auto scale-75 animate-pulse">
+                        <div className="w-12 h-12 rounded-full bg-gray-300 flex items-center justify-center"></div>
 
-                        <div className='col-span-3 text-left'>
-                          <div className='h-5 bg-gray-300 rounded w-3/4 mb-1'></div>
-                          <div className='h-4 bg-gray-200 rounded w-1/2'></div>
+                        <div className="col-span-3 text-left">
+                          <div className="h-5 bg-gray-300 rounded w-3/4 mb-1"></div>
+                          <div className="h-4 bg-gray-200 rounded w-1/2"></div>
                         </div>
 
-                        <div className='col-span-2 flex justify-end'>
-                          <div className='h-8 bg-gray-300 rounded w-24'></div>
+                        <div className="col-span-2 flex justify-end">
+                          <div className="h-8 bg-gray-300 rounded w-24"></div>
                         </div>
                       </div>
                     </div>
@@ -210,22 +208,24 @@ const Navbar: React.FC = () => {
                     !isSearchLoading &&
                     searchedUsers.length === 0 && (
                       <>
-                        <h1 className='col-span-6 text-center text-gray-500 font-semibold'>
+                        <h1 className="col-span-6 text-center text-gray-500 font-semibold">
                           No results found!
                         </h1>
                         <Link
                           onClick={() => {
                             setSearchedUsers([]);
-                            setInputValue('');
+                            setInputValue("");
                           }}
                           href={{
                             pathname: `/search-more-users`,
                             query: { userSearch: inputValue },
                           }}
                         >
-                          <span className='text-gray-400 hover:text-gray-700 custom-hover font-semibold'>
-                            More results
-                          </span>
+                          {searchedUsers.length !== 0 && (
+                            <span className="text-gray-400 hover:text-gray-700 custom-hover font-semibold">
+                              More results
+                            </span>
+                          )}
                         </Link>
                       </>
                     )}
@@ -234,14 +234,14 @@ const Navbar: React.FC = () => {
                     <Link
                       onClick={() => {
                         setSearchedUsers([]);
-                        setInputValue('');
+                        setInputValue("");
                       }}
                       href={{
                         pathname: `/search-more-users`,
                         query: { userSearch: inputValue },
                       }}
                     >
-                      <span className='text-gray-400 hover:text-gray-700 custom-hover font-semibold'>
+                      <span className="text-gray-400 hover:text-gray-700 custom-hover font-semibold">
                         More results
                       </span>
                     </Link>
@@ -250,15 +250,15 @@ const Navbar: React.FC = () => {
               )}
             </div>
           </div>
-          <HiOutlineSearch className='text-gray-100 text-3xl md:hidden cursor-pointer' />
+          <HiOutlineSearch className="text-gray-100 text-3xl md:hidden cursor-pointer" />
 
           <div>
             {user?.user_name && <AuthUserNavMenu />}
             {!user?.user_name && !isAuthLoading && (
-              <Link href={`login`} className='w-full'>
+              <Link href={`login`} className="w-full">
                 <PrimaryBtn
-                  text='Login'
-                  width='100%'
+                  text="Login"
+                  width="100%"
                   disabled={false}
                   icon={IoMdLogIn}
                 />
