4444dc03aa038ade164a0f2ebd1de861a8c1c26a
Dominic009 <dpg11000@gmail.com>
2024-12-09 12:59:55 +0600

    Fixed a bug for new user login

--- src/app/(Pages)/verifyEmail/page.tsx
+++ src/app/(Pages)/verifyEmail/page.tsx
@@ -1,44 +1,44 @@
-'use client';
-import { useForm } from 'react-hook-form';
-import PrimaryBtn from '@/components/PrimaryBtn';
-import React, { useState, useEffect } from 'react';
-import { useRouter } from 'next/navigation';
-import { AxiosError } from 'axios';
-import { resendEmailVerifyOtp, verifyUserEmail } from '@/api/account/account';
-import useLocalStorage from '@/hooks/useLocalStorage';
-import toast from 'react-hot-toast';
-import OTPInput from 'react-otp-input';
-import PrivateRoute from '@/components/PrivateRoute/PrivateRoute';
-import { useAuth } from '@/context/AuthContext/AuthProvider';
-import { loginUser } from '@/api/auth/auth';
-import Loader from '@/app/loading';
+"use client";
+import { useForm } from "react-hook-form";
+import PrimaryBtn from "@/components/PrimaryBtn";
+import React, { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { AxiosError } from "axios";
+import { resendEmailVerifyOtp, verifyUserEmail } from "@/api/account/account";
+import useLocalStorage from "@/hooks/useLocalStorage";
+import toast from "react-hot-toast";
+import OTPInput from "react-otp-input";
+import PrivateRoute from "@/components/PrivateRoute/PrivateRoute";
+import { useAuth } from "@/context/AuthContext/AuthProvider";
+import { loginUser, logoutUser } from "@/api/auth/auth";
+import Loader from "@/app/loading";
 
 const Page = () => {
   const router = useRouter();
-  const { item: accessToken } = useLocalStorage('auth-token');
-  const [otp, setOtp] = useState<string>('');
+  const { item: accessToken } = useLocalStorage("auth-token");
+  const [otp, setOtp] = useState<string>("");
   const [isOtpVerifyLoading, setOtpVerifyLoading] = useState<boolean>(false);
   const [isOtpResendLoading, setOtpResendLoading] = useState<boolean>(false);
-  const { user, isAuthLoading, login } = useAuth();
+  const { user, isAuthLoading, login, logout } = useAuth();
 
   const {
     register,
     handleSubmit,
     setValue,
     formState: { errors },
-  } = useForm({ mode: 'onSubmit' });
+  } = useForm({ mode: "onSubmit" });
 
   // Register the OTP field manually
   useEffect(() => {
-    register('otp', {
-      required: 'OTP is required',
+    register("otp", {
+      required: "OTP is required",
       minLength: {
         value: 6,
-        message: 'OTP must be 6 characters',
+        message: "OTP must be 6 characters",
       },
       maxLength: {
         value: 6,
-        message: 'OTP must be 6 characters',
+        message: "OTP must be 6 characters",
       },
     });
   }, [register]);
@@ -51,14 +51,21 @@ const Page = () => {
     const userData = {
       otp,
     };
-    console.log(userData, 'userData');
+    console.log(userData, "userData");
     try {
       const { data, status } = await verifyUserEmail(userData, lsItem);
       console.log(data);
       console.log(status);
-      login({ ...user, email_verified_at: true });
-      router.push('home');
-      toast.success(data.message);
+
+      if (status === 200) {
+        login({ ...user, email_verified_at: true });
+        toast.success("Email has been verified. Please log in again!");
+        logout();
+        console.log(data.data.access_token);
+      }
+
+      // router.push("home");
+      // toast.success(data.message);
     } catch (e) {
       const error = e as AxiosError<any>;
       console.log(error);
@@ -90,82 +97,82 @@ const Page = () => {
 
   if (isAuthLoading) {
     return (
-      <div className='h-[90vh] flex justify-center items-center'>
+      <div className="h-[90vh] flex justify-center items-center">
         <Loader></Loader>
       </div>
     );
   }
 
   if (!user) {
-    return router.push('login');
+    return router.push("login");
   }
 
   if (user.user_name && user.email_verified_at) {
-    return router.push('/home');
+    return router.push("/home");
   }
 
   return (
-    <main className='flex min-h-screen bg-gradient-to-b from-secondary to-[#00B4DB]'>
+    <main className="flex min-h-screen bg-gradient-to-b from-secondary to-[#00B4DB]">
       <div
-        className='grid grid-cols-1 gap-24 w-full items-center justify-center'
+        className="grid grid-cols-1 gap-24 w-full items-center justify-center"
         style={{
           backgroundImage: `url('/Pattern 3.png')`,
-          backgroundSize: '1700px',
-          backgroundPosition: '-600px -150px',
-          backgroundRepeat: 'no-repeat',
+          backgroundSize: "1700px",
+          backgroundPosition: "-600px -150px",
+          backgroundRepeat: "no-repeat",
         }}
       >
-        <div className='w-[30%] mx-auto z-50 -mt-24 md:-mt-0 animate__animated animate__fadeIn'>
-          <div className='bg-gray-900/30 py-10 rounded-xl flex flex-col items-center backdrop-blur-sm'>
-            <h1 className='text-3xl text-white border-b-4 border-[#008EAD] mb-8'>
+        <div className="w-[30%] mx-auto z-50 -mt-24 md:-mt-0 animate__animated animate__fadeIn">
+          <div className="bg-gray-900/30 py-10 rounded-xl flex flex-col items-center backdrop-blur-sm">
+            <h1 className="text-3xl text-white border-b-4 border-[#008EAD] mb-8">
               Verify Email
             </h1>
 
             <form
               onSubmit={handleSubmit(handleOtpSubmit)}
-              className='flex flex-col gap-5 w-[80%] items-center'
+              className="flex flex-col gap-5 w-[80%] items-center"
             >
               <OTPInput
                 value={otp}
                 onChange={(value: string) => {
                   setOtp(value);
-                  setValue('otp', value); // Sync with React Hook Form
+                  setValue("otp", value); // Sync with React Hook Form
                 }}
                 numInputs={6}
                 inputStyle={{
-                  width: '2.5rem',
-                  height: '2.5rem',
-                  margin: '0.5rem',
-                  fontSize: '1rem',
-                  borderRadius: '8px',
-                  border: '1px solid rgba(0,0,0,0.3)',
+                  width: "2.5rem",
+                  height: "2.5rem",
+                  margin: "0.5rem",
+                  fontSize: "1rem",
+                  borderRadius: "8px",
+                  border: "1px solid rgba(0,0,0,0.3)",
                 }}
-                renderInput={props => <input {...props} />}
+                renderInput={(props) => <input {...props} />}
               />
 
               {errors.otp && (
-                <span className='text-red-300'>
+                <span className="text-red-300">
                   {String(errors.otp.message)}
                 </span>
               )}
 
-              <div className='flex gap-5 w-full items-center'>
+              <div className="flex gap-5 w-full items-center">
                 <PrimaryBtn
                   text={`Submit OTP`}
                   disabled={isOtpVerifyLoading}
-                  width={'100%'}
-                  size={'xl'}
-                  weight={'semibold'}
-                  type='submit'
+                  width={"100%"}
+                  size={"xl"}
+                  weight={"semibold"}
+                  type="submit"
                   isLoading={isOtpVerifyLoading}
                 />
                 <PrimaryBtn
                   text={`Resend OTP`}
                   disabled={isOtpResendLoading}
-                  width={'100%'}
-                  size={'xl'}
-                  weight={'semibold'}
-                  type='button'
+                  width={"100%"}
+                  size={"xl"}
+                  weight={"semibold"}
+                  type="button"
                   isLoading={isOtpResendLoading}
                   onclick={() => handleOtpResend()}
                 />
